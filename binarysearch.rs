254565c6fb2adc29d2d6426b933135aa128ed0d1
Muhie <85196209+Muhie@users.noreply.github.com>
2022-06-08 00:47:16 +0100

    Binary Search!
    First index working..

--- binarysearch.rs
+++ binarysearch.rs
@@ -8,26 +8,29 @@ fn main(){
     binary_search(user_input);
 }
 fn binary_search(mut search_val: i32){
-    let mut my_array: [i32; 7] = [10,3,74,2,88,89,12];
+    let mut my_array: [i32; 7] = [11,3,74,2,88,89,12];
     println!("the value to be searched is: {}",search_val);
     let mut lower_bound = 0;
-    let mut upperbound = my_array.len();
+    let mut upper_bound = my_array.len();
     let mut mid_point = 0;
+    let mut first_index = 0;
     while my_array[mid_point] != search_val{
-        if lower_bound > upperbound{
-            println!("{}",upperbound);
+        if lower_bound > upper_bound{
+            println!("{}",upper_bound);
             println!("The entered value is not in the array {:?}", my_array);
+            first_index = first_index + 1;
             break;
         }
         else{
-            mid_point = lower_bound + (upperbound - lower_bound)/2
+            mid_point = lower_bound + (upper_bound - lower_bound)/2
         }
         if my_array[mid_point] == search_val{
-            println!("The entered value was found in the array at position {}",mid_point)
+            println!("The entered value was found in the array at position {}",mid_point);
+            first_index = first_index + 1;
         }
         else{
             if search_val < my_array[mid_point]{
-                upperbound = mid_point-1;
+                upper_bound = mid_point-1;
             }
             else{
                 lower_bound = mid_point + 1
@@ -35,4 +38,9 @@ fn binary_search(mut search_val: i32){
         }
 
     }
+    if first_index == 0{
+        println!("The entered value was found in the array at position {}",mid_point)
+    }
+
+
 }
